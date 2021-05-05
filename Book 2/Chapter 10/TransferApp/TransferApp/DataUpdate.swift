ae0288d8cc4e9db87adb87e2944195e11868f034
Vasily Usov <alkal@ya.ru>
2021-05-05 10:45:16 +0300

    Add new Chapters

---
+++ Book 2/Chapter 10/TransferApp/TransferApp/DataUpdate.swift
@@ -0,0 +1,12 @@
+//
+//  DataTransfer.swift
+//  TransferApp
+//
+//  Created by Василий Усов on 14.02.2021.
+//
+
+import Foundation
+
+protocol DataUpdateProtocol  {
+    func onDataUpdate(data: String)
+}
